189ddf27bfd50060157c90c28f76a3a98b76607e
Javier Martínez Ruiz <javiermaruiz@hotmail.com>
2020-10-03 11:05:57 +0200

    8

---
+++ src/unidad2/ejer_4_3_2_1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidad2;
+
+/**
+ *
+ * @author javie
+ */
+public class ejer_4_3_2_1 {
+    
+       public static void main(String[] args) {
+         
+           System.out.println("La tabla del 7 con la estructurta while");
+         int i=1;
+         
+         while(i<=10){
+             
+             int n=7*i;
+             System.out.println("7*"+i +"="+n);
+             i++;
+         }
+             
+     }
+    
+    
+}
