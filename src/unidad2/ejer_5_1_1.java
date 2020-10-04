c6e437c324fc4fd67c7cb2e5aad63f0734dda2cf
Javier Martínez Ruiz <javiermaruiz@hotmail.com>
2020-10-04 10:43:10 +0200

    8

---
+++ src/unidad2/ejer_5_1_1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidad2;
+
+/**
+ *
+ * @author javie
+ */
+public class ejer_5_1_1 {
+    
+    public static void main(String[] args) {
+        
+        int i=9;
+       
+        while(i>0){
+            
+        System.out.print("*");
+        
+        //||i==2+3*count
+        if(i==8||i==5||i==2){
+            i--;
+            continue;
+        }
+        System.out.println();
+        i--;
+   
+        }
+    }
+}
+    
+    
+
